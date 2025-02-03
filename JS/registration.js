4bf5a4d1f74d44a1a8592fed26abd9ee029f6e62
Shax <chester.daroy@gmail.com>
2025-02-03 21:59:18 +0800

    registration update v2

--- JS/registration.js
+++ JS/registration.js
@@ -1,5 +1,6 @@
 var pass = document.getElementById('pass');
 var eye = document.getElementById('eye');
+var eye2 = document.getElementById('eye2')
 
 eye.addEventListener('click', togglePass);
 eye2.addEventListener('click', togglePass1);
@@ -8,4 +9,10 @@ function togglePass(){
     eye.classList.toggle('active');
 
     (pass.type == 'password') ? pass.type = 'text' : pass.type = 'password';
+}
+
+function togglePass1(){
+    eye2.classList.toggle('active');
+
+    (repass.type == 'password') ? repass.type = 'text' : repass.type = 'password';
 }
