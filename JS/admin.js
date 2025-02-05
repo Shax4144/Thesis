3634c0785d45655153b7a26e10ff166af30ae4bb
Zeisulam <candovince0908@gmail.com>
2025-02-05 21:54:57 +0800

    dsa

--- JS/admin.js
+++ JS/admin.js
@@ -1,12 +1,71 @@
-function navbarDropdown(){
-    const dropdown = document.getElementById('drop-down');
-    if (dropdown.style.display === 'block'){
-        dropdown.style.display = 'none';
 
-    }
-    else{
-        dropdown.style.display = 'block';
-        
+document.addEventListener("DOMContentLoaded", function () {
+ 
+    const dashboard = document.querySelector(".dashboard");
+    const profile = document.querySelector(".profile");
+    const history = document.querySelector(".history");
+    const record = document.querySelector(".record");
+    const news = document.querySelector(".news");
+   
+
+    const dashboardContent = document.getElementById("dashboard-content");
+    const profileContent = document.getElementById("profile-content");
+    const historyContent = document.getElementById("history-content");
+    const recordContent = document.getElementById("record-content");
+    const newsContent = document.getElementById("news-content");
+   
+
+   
+    function hideAllContent() {
+        dashboardContent.classList.remove("active");
+        profileContent.classList.remove("active");
+        historyContent.classList.remove("active");
+        recordContent.classList.remove("active");
+        newsContent.classList.remove("active");
+    
+
+        dashboard.classList.remove("active");
+        profile.classList.remove("active");
+        history.classList.remove("active");
+        record.classList.remove("active");
+        news.classList.remove("active");
+     
     }
 
-}
+
+    hideAllContent();
+    dashboardContent.classList.add("active");
+    dashboard.classList.add("active");
+
+   
+    dashboard.addEventListener("click", function () {
+        hideAllContent();
+        dashboardContent.classList.add("active");
+        dashboard.classList.add("active");
+    });
+    
+    profile.addEventListener("click", function () {
+        hideAllContent();
+        profileContent.classList.add("active");
+        profile.classList.add("active");
+    });
+
+    history.addEventListener("click", function () {
+        hideAllContent();
+        historyContent.classList.add("active");
+        history.classList.add("active");
+    });
+    record.addEventListener("click", function () {
+        hideAllContent();
+        recordContent.classList.add("active");
+        record.classList.add("active");
+    });
+
+    news.addEventListener("click", function () {
+        hideAllContent();
+        newsContent.classList.add("active");
+        news.classList.add("active");
+    });
+
+    
+});
